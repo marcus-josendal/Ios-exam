bc587fac5bf1057cc5c924edfaad0053f8117fe5
Marcus Jøsendal <marcusjosendal@Marcuss-MacBook-Pro.local>
2019-10-10 11:09:39 +0200

    initial commit

---
+++ PG5600_5031/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PG5600_5031
+//
+//  Created by Marcus Jøsendal on 10/10/2019.
+//  Copyright © 2019 Marcus Jøsendal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
